bc24550a4ae52fc2679760ae03919a5aaafb37f8
kala888 <kala.niubility@gmail.com>
2020-05-21 13:56:32 +0800

    update home to test-case

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -1,77 +1,27 @@
 import React from 'react'
-import EleCarousel from '@/components/elements/ele-carousel'
-import ActionFloor from '@/components/navigation/action-floor'
-import SectionBar from '@/components/section-bar/section-bar'
-import Listof from '@/listof/listof'
-import MockService from '@/nice-router/request/mock-service'
-import { usePageTitle, usePullDown } from '@/service/use.service'
-import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 
-import { useSelector } from 'react-redux'
-import mockForm1Data from '../biz/mock-data/mock-form.data'
-import mockForm2Data from '../biz/mock-data/mock-form2.data'
-import mockGenericPageData from '../biz/mock-data/mock-genericpage.data'
-import { mockProductList, mockUserList } from '../biz/mock-data/mock-list.data'
-
+import { Button, Text, View } from '@tarojs/components'
 import './home.scss'
 
-function HomePage(props) {
-  const root = useSelector((state) => state.home)
-  const { pageTitle } = root
-  usePageTitle(pageTitle)
-  usePullDown(props)
 
-  // useAsyncEffect(() => NavigationService.view('page:///pages/biz/listof-test-page'))
-
-  const { slideList = mockUserList, actionList = defaultActionList } = root
+function HomePage() {
 
   return (
     <View className='home-page'>
-      <EleCarousel className='home-page-carousel' items={slideList} />
-      <View className='home-page-action-floor'>
-        <ActionFloor actions={actionList} />
-        <SectionBar title='促销抢购' linkToUrl='page:///pages/biz/listof-test-page' />
-        <Listof list={mockProductList} displayMode='product' />
-      </View>
+      <Text className='home-page-txt'> h5 bug1：这个字你不应该看的见才对</Text>
+      <Button
+        type='primary'
+        onClick={() => {
+          Taro.navigateTo('/page/me/me-page').then(() => {
+            console.log('Taro.navigateTo等方法没有then。。')
+          })
+        }}
+      >
+        h5bug2: 点这不应该报错
+      </Button>
     </View>
   )
 }
 
 export default HomePage
-
-MockService.mockResp('mock-generic-page/', mockGenericPageData)
-MockService.mockResp('mock-generic-form/', mockForm1Data)
-MockService.mockResp('mock-generic-form-2/', mockForm2Data)
-
-const defaultActionList = [
-  {
-    id: 1,
-    title: 'Listof 测试',
-    brief: '有惊喜',
-    linkToUrl: 'page:///pages/biz/listof-test-page',
-  },
-  {
-    id: 2,
-    title: 'GenericPage 测试',
-    brief: '省心',
-    linkToUrl: 'mock-generic-page/',
-  },
-  {
-    id: 3,
-    title: 'GenericForm 测试',
-    brief: '牛逼',
-    linkToUrl: 'mock-generic-form/',
-  },
-  {
-    id: 4,
-    title: 'H5 测试',
-    brief: '某度',
-    linkToUrl: 'https://www.baidu.com',
-  },
-  {
-    id: 5,
-    title: 'Login 页面',
-    brief: '通用',
-    linkToUrl: 'page:///pages/login/login-page',
-  },
-]
